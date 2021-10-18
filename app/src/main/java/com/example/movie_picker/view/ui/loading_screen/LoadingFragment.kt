58fec2c2feb572d45d1a3c45d50d8aac398342ac
Egor Gorbachev <e.gorbachev.vironit@vironit.ru>
2021-10-18 17:30:41 +0300

    feat: add ui for loading, login and register screens

--- app/src/main/java/com/example/movie_picker/view/ui/loading_screen/LoadingFragment.kt
+++ app/src/main/java/com/example/movie_picker/view/ui/loading_screen/LoadingFragment.kt
@@ -1,9 +1,31 @@
 package com.example.movie_picker.view.ui.loading_screen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.movie_picker.R
 import com.example.movie_picker.view.base.BaseFragment
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.lang.annotation.Native
 
 class LoadingFragment : BaseFragment(R.layout.fragment_loading) {
-
+	
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		
+		GlobalScope.launch {
+			delay(3000)
+			Navigation.findNavController(requireView()).navigate(R.id.action_loadingFragment_to_authenticationFragment)
+		}
+		
+		return super.onCreateView(inflater, container, savedInstanceState)
+	}
 }
---
+++ app/src/main/java/com/example/movie_picker/view/ui/registration_screen/RegistrationFragment.kt
@@ -0,0 +1,8 @@
+package com.example.movie_picker.view.ui.registration_screen
+
+import com.example.movie_picker.R
+import com.example.movie_picker.view.base.BaseFragment
+
+class RegistrationFragment : BaseFragment(R.layout.fragment_registration) {
+
+}

ba53be5474e569423ac905455f978e79de23ba59
Egor Gorbachev <e.gorbachev.vironit@vironit.ru>
2021-10-18 12:12:02 +0300

    init project

---
+++ app/src/main/java/com/example/movie_picker/view/ui/authentication_screen/AuthenticationFragment.kt
@@ -0,0 +1,8 @@
+package com.example.movie_picker.view.ui.authentication_screen
+
+import com.example.movie_picker.R
+import com.example.movie_picker.view.base.BaseFragment
+
+class AuthenticationFragment : BaseFragment(R.layout.fragment_authentication) {
+
+}
---
+++ app/src/main/java/com/example/movie_picker/view/ui/loading_screen/LoadingFragment.kt
@@ -0,0 +1,9 @@
+package com.example.movie_picker.view.ui.loading_screen
+
+import androidx.fragment.app.Fragment
+import com.example.movie_picker.R
+import com.example.movie_picker.view.base.BaseFragment
+
+class LoadingFragment : BaseFragment(R.layout.fragment_loading) {
+
+}
